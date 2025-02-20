03270672c6b44628b9afa2312a651014bafbb003
rjuntivero <jirokuntivero@gmail.com>
2025-02-20 03:58:24 -0500

    style: adjusted media query logic for sliding sidebar and added toggle button for sidebar

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -1,23 +1,19 @@
 import { useState, useEffect } from 'react'
-import { useQuery } from '@tanstack/react-query'
-import { Link } from 'react-router-dom'
 import Navbar from '../components/layout/Navbar'
 import Searchbar from '../components/ui/Searchbar'
 import Sidebar from '../components/layout/Sidebar'
 import ProductCard from '../components/ProductCard'
 import Footer from '../components/layout/Footer'
-import LoadIcon from '../components/icons/LoadIcon'
+import LoadWheel from '../components/ui/LoadWheel'
+import ErrorMessage from '../components/ui/ErrorMessage'
+import { Product } from '../types/Product'
+import { useFetchProducts } from '../hooks/useFetchProducts'
+import { useWindowSize } from '../hooks/useWindowSize'
 
 const Catalog = () => {
-  interface Product {
-    id: Number
-    title: string
-    category: string
-    image: string
-  }
-
   const [search, setSearch] = useState('')
   const [products, setProducts] = useState<Product[]>([])
+  const [showSidebar, setShowSidebar] = useState(true)
 
   const fetchProducts = async (): Promise<Product[]> => {
     await new Promise((resolve) => setTimeout(resolve, 1500))
@@ -25,58 +21,60 @@ const Catalog = () => {
     return response.json()
   }
 
-  const { data, isLoading, error } = useQuery<Product[]>({
-    queryKey: ['products'],
-    queryFn: fetchProducts,
-  })
+  const { data, isLoading, error } = useFetchProducts()
+
+  const { screenWidth } = useWindowSize()
 
   useEffect(() => {
     if (data) setProducts(data)
   }, [data])
 
+  useEffect(() => {
+    setShowSidebar(screenWidth > 1024)
+  }, [screenWidth])
+
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value)
+  }
+
+  const handleShowSideBar = () => {
+    setShowSidebar(!showSidebar)
   }
 
   return (
     <>
       <Navbar />
       <div
-        className="wrapper grid h-full min-h-screen gap-x-9.5 pr-3 md:pr-3"
+        className="wrapper grid h-full min-h-screen gap-x-9.5 pr-3 transition-all duration-500 md:pr-3"
         style={{
-          gridTemplateColumns:
-            'minmax(325px, 325px) minmax(600px, clamp(600px, 100%, 1200px))',
+          gridTemplateColumns: showSidebar ? '325px' : '0px',
         }}
       >
-        <div className="sticky top-12 row-start-2 hidden self-start lg:block">
+        <div
+          className={`sticky top-12 row-start-2 self-start transition-transform duration-500 ${
+            showSidebar ? 'lg:translate-x-0' : '-translate-x-full'
+          }`}
+          style={{
+            width: '325px',
+          }}
+        >
           <Sidebar />
         </div>
 
-        <div className="bg-background-light sticky top-0 z-2 col-span-1 col-start-2 row-start-1 py-5 pr-2 pl-2">
+        <div className="bg-background-light sticky top-0 z-2 col-span-1 col-start-2 row-start-1 py-5 pr-2 pl-12">
           <Searchbar
             input={search}
             onChange={handleSearch}
             results={products.length}
+            sideBarVisible={showSidebar}
+            showSidebar={handleShowSideBar}
           />
         </div>
 
-        <div className="catalog col-span-1 row-start-2 h-full transition-all duration-500">
+        <div className="catalog col-span-1 col-start-2 row-start-2 h-full transition-all duration-500">
           <div className="product-container flex min-h-[800px] flex-wrap justify-center gap-6">
-            {isLoading && (
-              <div className="flex flex-col items-center justify-center gap-3">
-                <p className="font-big-header text-primary-light flex justify-center text-2xl">
-                  Loading Products
-                </p>
-                <LoadIcon />
-              </div>
-            )}
-            {error && (
-              <div className="flex flex-col items-center justify-center">
-                <p className="font-big-header text-primary-light flex justify-center text-2xl">
-                  Something went wrong. Please try again later.
-                </p>
-              </div>
-            )}
+            {isLoading && <LoadWheel />}
+            {error && <ErrorMessage />}
             {!isLoading &&
               !error &&
               products.map((product) => (
